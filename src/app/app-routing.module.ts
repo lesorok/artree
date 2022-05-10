59f4ae4f7a188b492bf96196c24883e455fe302f
lena <sorochinaelena2001@gmail.com>
2022-05-10 17:37:31 +0300

    Кек

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,18 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 
 const routes: Routes = [
-  {path: 'category', loadChildren: () => import('./category/category.module').then(m => m.CategoryModule) },
-  {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
-  {path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule)},
-  {path: 'profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)},
+  {path: 'category', loadChildren: () => import('./routing/category/category.module').then(m => m.CategoryModule) },
+  {path: 'login', loadChildren: () => import('./routing/user/login/login.module').then(m => m.LoginModule)},
+  {path: 'register', loadChildren: () => import('./routing/user/register/register.module').then(m => m.RegisterModule)},
+  {path: 'profile', loadChildren: () => import('./routing/user/profile/profile.module').then(m => m.ProfileModule)},
   {path: 'timeline', loadChildren: () => import('./timeline/timeline.module').then(m => m.TimelineModule)},
   {path: 'admin-mode', loadChildren: () => import('./admin-mode/admin-mode.module').then(m => m.AdminModeModule)},
+  {path: "**", loadChildren: () => import('./routing/not-found/not-found.module').then(m => m.NotFoundModule)},
   {path: '', pathMatch: 'full', redirectTo: 'category'},
 ]
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
   ],
   exports: [
     RouterModule
