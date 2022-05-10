1bbb024bedfb7045c0444f1cc647302aaa8b5b67
lena <sorochinaelena2001@gmail.com>
2022-05-10 18:53:19 +0300

    Еще один апргейт

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,17 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 
 const routes: Routes = [
-  {path: 'category', loadChildren: () => import('./routing/category/category.module').then(m => m.CategoryModule) },
-  {path: 'login', loadChildren: () => import('./routing/user/login/login.module').then(m => m.LoginModule)},
-  {path: 'register', loadChildren: () => import('./routing/user/register/register.module').then(m => m.RegisterModule)},
-  {path: 'profile', loadChildren: () => import('./routing/user/profile/profile.module').then(m => m.ProfileModule)},
-  {path: 'timeline', loadChildren: () => import('./timeline/timeline.module').then(m => m.TimelineModule)},
-  {path: 'admin-mode', loadChildren: () => import('./admin-mode/admin-mode.module').then(m => m.AdminModeModule)},
-  {path: "**", loadChildren: () => import('./routing/not-found/not-found.module').then(m => m.NotFoundModule)},
-  {path: '', pathMatch: 'full', redirectTo: 'category'},
+  {path: 'categories', loadChildren: () => import('./componets/categories/categories-page/categories-page.module').then(m => m.CategoriesPageModule) },
+  {path: 'login', loadChildren: () => import("./componets/user/login/login-page/login-page.module").then(m => m.LoginPageModule)},
+  {path: 'register', loadChildren: () => import('./componets/user/register/register-page/register-page.module').then(m => m.RegisterPageModule)},
+  {path: 'profile', loadChildren: () => import('./componets/user/profile/profile-page/profile-page.module').then(m => m.ProfilePageModule)},
+  {path: 'feed', loadChildren: () => import('./componets/feed/feed-page/feed-page.module').then(m => m.FeedPageModule)},
+  {path: 'admin', loadChildren: () => import('./componets/admin/admin-page/admin-page.module').then(m => m.AdminPageModule)},
+  {path: "**", loadChildren: () => import('./componets/not-found/not-found-routing.module').then(m => m.NotFoundRoutingModule)},
+  {path: '', pathMatch: 'full', redirectTo: 'categories'},
 ]
 @NgModule({
+
   imports: [
     RouterModule.forRoot(routes),
   ],
